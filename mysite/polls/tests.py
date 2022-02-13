1c41ade126abf2ad47fe89ac8cae9c471f779b2c
ahmadf <fanaei.ahmad@gmail.com>
2022-02-14 02:44:49 +0330

    create data for n+1 issue and change code to trigger it

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -123,5 +123,7 @@ class QuestionDetailViewTests(TestCase):
         """
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
-        response = self.client.get(url)
+
+        with self.assertNumQueries(1):
+            response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
