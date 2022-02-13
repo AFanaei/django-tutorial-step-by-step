04ce2b55a2039bb89aab20159152c899c79b6303
ahmadf <fanaei.ahmad@gmail.com>
2022-02-14 01:15:24 +0330

    Create dockerfile and docker compose

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # SQLite is fine for now
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DEFAULT_DB_CONFIG = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': DEFAULT_DB_CONFIG}
 
 
 # Password validation
@@ -119,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
